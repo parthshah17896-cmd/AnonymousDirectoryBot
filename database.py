995568020113aa2fca15f096a269cb0c3e78bce4
parthshah17896-cmd <parthshah17896@gmail.com>
2026-07-26 01:28:55 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,8 +3,12 @@ import sqlite3
 DB_NAME = "users.db"
 
 
+def get_connection():
+    return sqlite3.connect(DB_NAME)
+
+
 def init_db():
-    conn = sqlite3.connect(DB_NAME)
+    conn = get_connection()
     cursor = conn.cursor()
 
     cursor.execute("""
@@ -18,3 +22,50 @@ def init_db():
 
     conn.commit()
     conn.close()
+
+
+def user_exists(telegram_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT 1 FROM users WHERE telegram_id = ?",
+        (telegram_id,)
+    )
+
+    result = cursor.fetchone()
+
+    conn.close()
+
+    return result is not None
+
+
+def save_selection(telegram_id, selected_name, selected_bot):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO users
+        (telegram_id, selected_name, selected_bot)
+        VALUES (?, ?, ?)
+    """, (telegram_id, selected_name, selected_bot))
+
+    conn.commit()
+    conn.close()
+
+
+def get_selection(telegram_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT selected_name, selected_bot
+        FROM users
+        WHERE telegram_id = ?
+    """, (telegram_id,))
+
+    result = cursor.fetchone()
+
+    conn.close()
+
+    return result
