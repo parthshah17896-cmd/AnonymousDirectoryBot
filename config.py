d97b9df6db500698462483e0f110efcbe42b7a15
parthshah17896-cmd <parthshah17896@gmail.com>
2026-07-27 19:23:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-print("BOT_TOKEN =", os.getenv("BOT_TOKEN"))
-print("ADMIN_ID =", os.getenv("ADMIN_ID"))
-
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
